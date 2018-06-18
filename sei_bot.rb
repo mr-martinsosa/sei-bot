1d5efdb7b57f9e57adbcadc89ed490681f25d71f
Martin Sosa <mr.martinsosa@gmail.com>
2018-06-18 11:12:27 -0400

    basic functionality working

--- sei_bot.rb
+++ sei_bot.rb
@@ -4,6 +4,11 @@ class SeiBot < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
     client.say(text: 'pong!', channel: data.channel)
   end
+
+  command 'pong' do |client, data, match|
+    client.say(text: 'I pong, you ping.', channel: data.channel)
+  end
+
 end
 
 SeiBot.run
